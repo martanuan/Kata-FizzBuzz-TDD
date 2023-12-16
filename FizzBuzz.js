31e1588522ba05c43355ed57cde805e131f2dfa6
martanuan <martanyp@hotmail.com>
2023-12-16 19:23:25 +0100

    Adding a new test

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,11 +1,13 @@
 class FizzBuzz{
     validar_primos_3_5(numero) {
-        if (numero %15 === 0 ){
+        if (numero %3 === 0 && numero %5 === 0 ){
             return "FizzBuzz";
         } else if (numero %3 === 0){
             return "Fizz";
-        }else if (numero %5 ===0) {
+        } else if (numero %5 ===0) {
             return "Buzz";
+        } else{
+            return numero
         }
     }
 
--- tests/FizzBuzz.test.js
+++ tests/FizzBuzz.test.js
@@ -75,6 +75,26 @@ describe('Tests que validan si los múltiplos de 3 y 5', () => {
 
     })
 
+    test('Test devuelve el número cuando no sea divisible por 3 ni 5', () => {
+        /**
+         * GHERKIN TEST (para plantear un escenario)
+            * GIVEN / ARRANGE: un número
+            * WHEN / ACT: cuando este sea divisible por 3 y 5
+            * THEN / ASSERT: devuelve FizzBuzz como resultado
+         */
+
+        // ARRANGE: preparamos el escenario
+        const numero_ingresado = 1;
+        const respuesta_esperada = numero_ingresado;
+
+        // ACT: ejecutar el escenario
+        const respuesta_recibida = fizz_buzz.validar_primos_3_5(numero_ingresado);
+
+        // ASSERT: comprobar el escenario
+        expect(respuesta_recibida).toBe(respuesta_esperada);
+
+    });
+
 
 
 
